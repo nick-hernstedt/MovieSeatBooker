a333fd5ac33d35f1079db4d40693a68b4ffe5fb9
copiousamounts <nick.hernstedt@gmail.com>
2020-02-12 15:30:38 -0800

    added event listeners for seat click and moive price change

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,4 +4,32 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("moive");
 
-const ticketPrice = +moiveSelect.value;
+let ticketPrice = +moiveSelect.value;
+
+// update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  let selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSEatsCount * ticketPrice;
+}
+
+// movie select event
+movieSelect.addEventListener("change", function(e) {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// seat click event
+container.addEventListener("click", function(e) {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
